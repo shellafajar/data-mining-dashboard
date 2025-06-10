2bdb0adb77b76e0f6214868d3ce0ee7b4d07b431
shellafajar <shellafajar@students.unnes.ac.id>
2025-06-10 11:44:36 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Data Mining", layout="wide")
+st.title("Dashboard Data Mining - Status Gizi Balita")
+st.markdown("Silakan gunakan menu di samping kiri untuk menavigasi halaman.")
